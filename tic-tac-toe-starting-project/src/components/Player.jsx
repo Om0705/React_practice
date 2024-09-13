9ca8716ce3f10d3151d72b1d20c1f1c47853510f
Om0705 <omsinghal1663@gmail.com>
2024-09-13 22:22:29 +0530

    .

--- tic-tac-toe-starting-project/src/components/Player.jsx
+++ tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 
-export default function Player({name , symbol}){
+export default function Player({name , symbol, isActive}){
     const [isEditing , setIsEditing] = useState(false);
     const [newName , setNewName] = useState(name);
 
@@ -20,7 +20,7 @@ export default function Player({name , symbol}){
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
           <span className="player">
           {playerName}
           <span className="player-symbol">{symbol}</span>
